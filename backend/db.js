70a1a921e9f1cf88329a16617675d85bbd1a115d
Kaushal Patel <kaushalpatel531.kp@gmail.com>
2024-07-26 16:54:10 +0530

    Create backend with express,node and connect db through mongodb.

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017/?readPreference=primary&appName=MongoDB%20Compass&directConnection=true&ssl=false";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI);
+    mongoose.connection.on('connected', function () {  
+        console.log('Database connected successfully');
+    }); 
+}
+
+
+module.exports = connectToMongo;
